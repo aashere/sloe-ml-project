71e1e49b4128a76583b20912992a4c1901466b49
Ameya Shere <ameya.shere@publicissapient.com>
2022-05-10 18:44:03 -0400

    expand rounding

--- analysis.py
+++ analysis.py
@@ -6,13 +6,13 @@ df = df.rename(columns={'ratio': 'kappa'})
 
 print('Different F1 scores:')
 f1_df = df[['n','kappa','type','baseline F1 score','SLOE F1 score']][df['baseline F1 score'] != df['SLOE F1 score']]
-f1_df = f1_df.round(2)
+f1_df = f1_df.round(4)
 f1_df.to_csv('results/tables/f1.csv', index=False, encoding='utf8')
 print(f1_df)
 
 print('SLOE is faster for:')
 time_df = df[['n','kappa','type','baseline time (s)','SLOE time (s)']][df['baseline time (s)'] > df['SLOE time (s)']]
-time_df = time_df.round(2)
+time_df = time_df.round(4)
 time_df.to_csv('results/tables/time.csv', index=False, encoding='utf8')
 print(time_df)
 
